3ad101dd482fc103d988a64132a9c050806fbee8
pooja bishnoi <poojabishnoi0919@gmail.com>
2025-07-26 11:35:45 +0530

    test

--- SpringBootRestfulProgram1/src/main/java/com/example/SpringBootRestfulProgram1/controller/MyController.java
+++ SpringBootRestfulProgram1/src/main/java/com/example/SpringBootRestfulProgram1/controller/MyController.java
@@ -14,41 +14,41 @@ public class MyController {
     @Autowired
     private EmpService empService;
 
-    //    @PostMapping("/employee")
-//    public Employee addEmployeeDetails(@RequestBody Employee employee){
-//
-//        return empService.createEmployee(employee);
-//    }
-//
-//    @GetMapping("/employee")
-//    public List<Employee> getAllEmployeeDetails() {
-//        return empService.getAllEmployee();
-//    }
-//
-//    @GetMapping("/employee/{id}")
-//    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
-//        Employee employee= empService.getEmployeeById(id).orElse(null);
-//        if(employee!= null) {
-//            return ResponseEntity.ok(employee);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//    @PutMapping("/employee/{id}")
-//    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
-//        Employee updatedEmployee = empService.updateEmployee(id, employee);
-//        if (updatedEmployee != null) {
-//            return ResponseEntity.ok(updatedEmployee);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//    @DeleteMapping("/employee/{id}")
-//    public ResponseEntity<Void> deleteEmployee(@PathVariable int id) {
-//        empService.deleteEmployee(id);
-//        return ResponseEntity.noContent().build();
-//    }
+        @PostMapping("/employee")
+    public Employee addEmployeeDetails(@RequestBody Employee employee){
+
+        return empService.createEmployee(employee);
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> getAllEmployeeDetails() {
+        return empService.getAllEmployee();
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
+        Employee employee= empService.getEmployeeById(id).orElse(null);
+        if(employee!= null) {
+            return ResponseEntity.ok(employee);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        Employee updatedEmployee = empService.updateEmployee(id, employee);
+        if (updatedEmployee != null) {
+            return ResponseEntity.ok(updatedEmployee);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable int id) {
+        empService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
     @GetMapping(value = "/employee", params = "name")
     public List<Employee> getEmployeesByName(@RequestParam String name) {
 
